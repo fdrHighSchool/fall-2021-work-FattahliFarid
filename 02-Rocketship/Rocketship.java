8960b8c47cd4e244face36ade7adf067c4bbcb87
Farid Fattahli <faridf@nycstudents.net>
2021-09-29 11:24:19 -0400

    final form for Rocketship

--- 02-Rocketship/Rocketship.java
+++ 02-Rocketship/Rocketship.java
@@ -1,9 +1,40 @@
 public class Rocketship {
   public static void main(String[] args) {
-System.out.println("    /  \\");
-System.out.println("   /    \\");
-System.out.println("  /      \\");
-System.out.println(" /        \\");
-System.out.println("/          \\");
+    triangle();
+
+    square();
+
+    System.out.println("|                |");
+    System.out.println("|                |");
+    System.out.println("| United States  |");
+    System.out.println("|                |");
+    System.out.println("|                |");
+    System.out.println("|                |");
+
+    square();
+
+    triangle();
+
   }
+
+  public static void triangle() {
+    System.out.println("       /  \\");
+    System.out.println("      /    \\");
+    System.out.println("     /      \\");
+    System.out.println("    /        \\");
+    System.out.println("   /          \\");
+
+
+
+ }
+   public static void square() {
+     System.out.println("+----------------+");
+     System.out.println("|                |");
+     System.out.println("|                |");
+     System.out.println("|                |");
+     System.out.println("|                |");
+     System.out.println("|                |");
+     System.out.println("|                |");
+     System.out.println("+----------------+");
+   }
 }
