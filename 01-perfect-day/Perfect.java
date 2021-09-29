4f3115e410a9b76d84f9e15cf395b6533bfdc2fe
Farid Fattahli <faridf@nycstudents.net>
2021-09-28 21:58:58 -0400

    Final commit for the whole assignment

--- 01-perfect-day/Perfect.java
+++ 01-perfect-day/Perfect.java
@@ -1,7 +1,15 @@
 public class Perfect {
   public static void main(String[] args) {
       System.out.println("Hello.Welcome to my Perfect day!");
-      System.out.print("My Morning schedule:");
-        System.out.print(" wake up and take a shower");
+      System.out.println("My Morning schedule:");
+      System.out.println("Wake up and take a shower.");
+      System.out.println("Brush my teeth and eat breakfeast.");
+      System.out.println("Go to school");
+      System.out.println("My noon schedule:");
+      System.out.println("Come back from school and chill/relax for a bit");
+      System.out.println("finish all of my homework");
+      System.out.println("My evening schedule:");
+      System.out.println("Eat food and play video games with my friend if i have extra time left");
+      System.out.println("And this is my Perfect day!");
      }// closes main class
   }// closes class
